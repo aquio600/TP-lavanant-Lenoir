a50be217e6f8718c131d3e59e2e6942e9560c699
Elo144 <130460841+Elo144@users.noreply.github.com>
2024-11-29 11:33:19 +0100

    update 1

--- untitled1/src/main/java/org/example/Aeroport.java
+++ untitled1/src/main/java/org/example/Aeroport.java
@@ -13,4 +13,32 @@ public class Aeroport {
         Avion.affecterVol();
     }
 
+    //constructeur
+    public Aeroport(String nom, String ville, String description) {
+        this.nom = nom;
+        this.ville = ville;
+        this.description = description;
+    }
+    //Getter
+    public String getNom() {
+        return nom;
+    }
+    public String getVille() {
+        return ville;
+    }
+    public String getDescription() {
+        return description;
+    }
+    //Setter
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
 }
--- untitled1/src/main/java/org/example/Vol.java
+++ untitled1/src/main/java/org/example/Vol.java
@@ -23,5 +23,53 @@ public class Vol {
         //...
     }
 
+    //constructeur
+    public Vol(int numeroVol, String origine, String destination, Date dateHeureDepart, Date dateHeureArrivee, String etat) {
+        this.numeroVol = numeroVol;
+        this.origine = origine;
+        this.destination = destination;
+        this.dateHeureDepart = dateHeureDepart;
+        this.dateHeureArrivee = dateHeureArrivee;
+        this.etat = etat;
+    }
+    //Getter
+    public int getNumeroVol() {
+        return numeroVol;
+    }
+    public String getOrigine() {
+        return origine;
+    }
+    public String getDestination() {
+        return destination;
+    }
+    public Date getDateHeureDepart() {
+        return dateHeureDepart;
+    }
+    public Date getDateHeureArrivee() {
+        return dateHeureArrivee;
+    }
+    public String getEtat() {
+        return etat;
+    }
+    //Setter
+    public void setNumeroVol(int numeroVol) {
+        this.numeroVol = numeroVol;
+    }
+    public void setOrigine(String origine) {
+        this.origine = origine;
+    }
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+    public void setDateHeureDepart(Date dateHeureDepart) {
+        this.dateHeureDepart = dateHeureDepart;
+    }
+    public void setDateHeureArrivee(Date dateHeureArrivee) {
+        this.dateHeureArrivee = dateHeureArrivee;
+    }
+    public void setEtat(String etat) {
+        this.etat = etat;
+    }
+
 
 }
