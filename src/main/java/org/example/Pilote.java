bd965b4767f6af858b62421730fd36b01d2c89d6
Aquio600 <lavanantclement23@gmail.com>
2024-11-29 10:20:30 +0100

    ajout classe personnel

---
+++ src/main/java/org/example/Pilote.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Pilote extends Employé {
+    public int Licence;
+    public int heuresVol;
+
+
+    public void ObtenirRole(){
+        System.out.println("Role = Pilote");
+    }
+
+    public void AffecterVol(){}
+    public void ObtenirVol(){}
+}
