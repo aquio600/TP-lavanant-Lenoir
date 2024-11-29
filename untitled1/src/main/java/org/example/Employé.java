1564b7c9fa5860c64e36437c548db623b47ecf3a
hello <lavanantclement23@gmail.com>
2024-11-29 10:21:31 +0100

    Add files via upload

---
+++ untitled1/src/main/java/org/example/Employé.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Employé extends Personne{
+    public int numeroemployé;
+    public String dateEmbauche;
+
+public void ObtenirRole(){
+    System.out.println("Role = void");
+}
+}
